953b0ac7be563df6a7d2e1619e698271a710803c
Sourabh <kr.sourabh17@gmail.com>
2019-11-20 22:49:27 +0100

    Word Count setup

--- BigData/src/RMIPractice/Client.java
+++ BigData/src/RMIPractice/Client.java
@@ -3,6 +3,13 @@ import java.rmi.*;
 
 public class Client {
     public static void main(String[] args) throws  Exception{
+        String serverIp = "localhost";
+        String serverPort = "1997";
+        String bname = "RemoteFunction";
+        String connectionLocation = "rmi://" + serverIp + ":" +  serverPort + "/" + bname;
+
+        FileProcInterface remoteObject =  (FileProcInterface)Naming.lookup(connectionLocation);
+        remoteObject.loadFile("/home/sourabh/Studies/Code/Java/BigData/Data/data.csv");
 
     }
 }
--- BigData/src/RMIPractice/Server.java
+++ BigData/src/RMIPractice/Server.java
@@ -1,9 +1,26 @@
 package RMIPractice;
 
+import java.rmi.RemoteException;
 import java.rmi.*;
 
 public class Server {
     public static void main(String[] args) throws RemoteException {
+
+        String serverIp = "localhost";
+        String serverPort = "1997";
+        String bname = "RemoteFunction";
+        String bindLocation = "rmi://" + serverIp + ":" +  serverPort + "/" + bname;
+
+        FileProc remoteMethods = new FileProc();
+        try{
+        Naming.bind(bindLocation, remoteMethods);
+        }
+        catch (Exception e){
+            System.out.println(e);
+            System.out.println("Error");
+
+        }
+        System.out.println("Server ready");
 
     }
 }
